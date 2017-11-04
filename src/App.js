ffe19f61432f902ea5b7e3c5b817bff8d7279b05
Catherine Kleimeier <catherine.kleimeier@gmail.com>
2017-11-04 17:48:20 -0400

    Create initial functionality
    Create very rough initial functionality,
    using one component, one function to
    get input and compile output, and
    dangerouslySetInnerHTML to
    render the compiled markdown.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+var marked = require('marked');
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      original: '',
+      compiled: ''
+    }
+  }
+
+  handleChange(event) {
+    this.setState({original: event.target.value});
+
+    this.setState({compiled: marked(this.state.original)});
+  }
+
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          Test
-        </p>
-      </div>
-    );
+      <section id="app">
+        <section id="input">
+          <textarea ref="inputArea" value={this.state.original? this.state.original : ''} onChange={this.handleChange.bind(this)}/>
+        </section>
+        <section id="output" dangerouslySetInnerHTML={{__html:this.state.compiled}}></section>
+      </section>
+    )
   }
 }
 
+
 export default App;
+
+/*
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+*/
